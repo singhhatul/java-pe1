4848f6cce6a80a309e90c5fe1150fdab964ce991
ATUL SINGH <atulsingh0622@gmail.com>
2019-07-12 12:15:22 +0530

    Adde test cases to class IntegerSumTest

--- src/main/java/com/stackroute/pe1/IntegerSum.java
+++ src/main/java/com/stackroute/pe1/IntegerSum.java
@@ -3,20 +3,32 @@ package com.stackroute.pe1;
 import java.util.Scanner;
 
 public class IntegerSum {
-        public static void main(String[] args) {
-            int sum = 0;
-            int num = 0;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter 0 when you want to exit");
-            do {
-                System.out.println("Enter the number");
-                num = sc.nextInt();
-                sum += num;
-            } while (num != 0);
-            System.out.println("Sum of the numbers is:" + sum);
-        }
+    public String  sumOfIntegerValue(String inputStringNumbers){
+        String[] arrayOfStringNumber = inputStringNumbers.split(" ");
+        boolean boo = true;
+        int sum = 0;
+        for (String str: arrayOfStringNumber) {
+            for (int i = 0; i < str.length(); i++){
+                if ( (!(Character.isDigit(str.charAt(i)))) &&
+                        (str.charAt(i) != '-') &&
+                        (str.charAt(i) != '+')){
+                    boo = false;
+                }
 
+            }
+        }
+        if (boo){
+            for (String str: arrayOfStringNumber){
+                sum += Integer.parseInt(str);
+            }
+            return String.valueOf(sum);
+        }
+        else {
+            return "Non Integer Value";
+        }
     }
+
+}
 
 
 
---
+++ src/test/java/com/stackroute/pe1/IntegerSumTest.java
@@ -0,0 +1,44 @@
+package com.stackroute.pe1;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IntegerSumTest {
+
+        IntegerSum integerAndNonIntegerValue;
+
+        @Before
+        public void setUp() {
+            //arrange
+            this.integerAndNonIntegerValue = new IntegerSum();
+        }
+
+        @Test
+        public void givenStringShouldReturnATotal() {
+            //act
+            String actualResult = this.integerAndNonIntegerValue.sumOfIntegerValue(
+                    "20 30 23 32");
+            //assert
+            assertEquals("105", actualResult);
+        }
+
+        @Test
+        public void givenStringShouldReturnAErrorMessage() {
+            //act
+            String actualResult = this.integerAndNonIntegerValue.sumOfIntegerValue(
+                    "20 fe 2e 32");
+            //assert
+            assertEquals("Non Integer Value", actualResult);
+        }
+
+
+        @Test
+        public void givenStringShouldReturnAZero() {
+            //act
+            String actualResult = this.integerAndNonIntegerValue.sumOfIntegerValue(
+                    "20 -20");
+            //assert
+            assertEquals("0", actualResult);
+        }
+    }
